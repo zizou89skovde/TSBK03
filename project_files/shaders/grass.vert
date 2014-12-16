486df0c867913a04e557c9b6400fcd64e0a1dfdc
joachimlarsson <joachimlarsson90@gmail.com>
2014-12-16 15:03:46 +0100

    galen
    s

--- project_files/shaders/grass.vert
+++ project_files/shaders/grass.vert
@@ -1,7 +1,7 @@
 #version 150
 in  vec3 in_Position;
 
-
+uniform mat4 MV_Matrix;
 
 uniform sampler2D u_HeightMap;
 uniform float u_GridHeightScale;
@@ -14,17 +14,27 @@ out float g_Height;
 
 void main(void)
 {
-	/* Set out texture coordinate */
+
+/*
+	// Set out texture coordinate
 	g_TextureCoord = in_Position.xy;
 	
-	/* Compute world coordinates */
-	vec3 position = vec3(in_Position.x,0.0,in_Position.z)*20.0 - 10.0; //u_GridSize + u_GridOffset;
+	// Compute world coordinates
+	vec3 position = vec3(in_Position.x,0.0,in_Position.z)*u_GridSize + u_GridOffset;
 	
-	/* Read vertex y-value (height) from heightmap */
-	float height = texture(u_HeightMap,in_Position.xz).x; //vec2(1.0-in_Position.xxxxxxx,1.0-in_Position.y)
-	/* Scale and bias */
-	g_Height = (height-0.5)*5.0; //u_GridHeightScale;
+	// Read vertex y-value (height) from heightmap
+	float height = texture(u_HeightMap,in_Position.xz).x; 
+	
+	// Scale and bias
+	g_Height = (height-0.5)*u_GridHeightScale;
 
+	// Output to next shader
 	gl_Position = vec4(position,1.0);
+
+*/	
+	vec4 position = vec4(in_Position,1.0);
+	position.y  = texture(u_HeightMap,in_Position.xz).r;
+	gl_Position = position;
+	
 }
 
--- project_files/src/GrassSimulation.cpp
+++ project_files/src/GrassSimulation.cpp
@@ -13,13 +13,13 @@ void GrassSimulation::initialize(){
     mGrassScene = new ModelObject(); //Notera: ModelObject är lite feldöpt. Den borde heta Scene object. Eftersom den kan hålla flera olika modeller/shaders osv.
 
 	/** Load shader **/
-	GLuint grassShader = loadShadersG("shaders/grass.vert", "shaders/grass.frag", "shaders/grass.gs");
-	//GLuint grassShader = loadShadersGT("shaders/grass.vert", "shaders/grass.frag", "shaders/grass.gs", "shaders/grass.tcs", "shaders/grass.tes");
+	//GLuint grassShader = loadShadersG("shaders/grass.vert", "shaders/grass.frag", "shaders/grass.gs");
+	GLuint grassShader = loadShadersGT("shaders/grass.vert", "shaders/grass.frag", "shaders/grass.gs", "shaders/grass.tcs", "shaders/grass.tes");
 	//GLuint grassShader = loadShaders("shaders/grass.vert", "shaders/grass.frag");
 
 	mGrassScene->setShader(grassShader, GRASS_SHADER_ID, MVP); //mGrassScene->setShader(grassShader, GRASS_SHADER_ID,VP);
 	mGrassScene->setTransform(IdentityMatrix(), GRASS_SHADER_ID);
-	//mGrassScene->setDrawMethod(PATCHES, GRASS_SHADER_ID);
+	mGrassScene->setDrawMethod(PATCHES, GRASS_SHADER_ID);
 
 
 	mEnvironment->setReflectedModels(mGrassScene,GRASS_SHADER_ID);
@@ -40,28 +40,41 @@ void GrassSimulation::initialize(){
 	/***** START WATCH OUT **********************************************************************************/
 
     /** Upload terrain parameters **/
-    EnvironmentMetaData metaData = mEnvironment->getMetaData();
+    //EnvironmentMetaData metaData = mEnvironment->getMetaData();
 
     /** Upload offset each grass vertex **/
+    /*
     GLfloat gridOffset[3];
     gridOffset[0] = -metaData.sSize[0]/2.0;
     gridOffset[1] = 0;
     gridOffset[2] = -metaData.sSize[2]/2.0;
-    mGrassScene->setUniformFloat(gridOffset,3,GRASS_SHADER_ID,"u_GridOffset");
+    */
+    //mGrassScene->setUniformFloat(gridOffset,3,GRASS_SHADER_ID,"u_GridOffset");
 
-    mGrassScene->setUniformFloat(metaData.sSize[0],GRASS_SHADER_ID,"u_GridSize");
-    mGrassScene->setUniformFloat(metaData.sSize[1],GRASS_SHADER_ID,"u_GridHeightScale");
-    mGrassScene->setTexture(metaData.sHeightMapHandle,GRASS_SHADER_ID,"u_HeightMap");
+   // mGrassScene->setUniformFloat(metaData.sSize[0],GRASS_SHADER_ID,"u_GridSize");
+    //mGrassScene->setUniformFloat(metaData.sSize[1],GRASS_SHADER_ID,"u_GridHeightScale");
+    //mGrassScene->setTexture(metaData.sHeightMapHandle,GRASS_SHADER_ID,"u_HeightMap");
     /** Set hight map texture **/
 
 	/***** END WATCH OUT **********************************************************************************/
 
+	/** Load texture **/
+    EnvironmentMetaData environmentMetaData = mEnvironment->getMetaData();
+	mGrassScene->setTexture(environmentMetaData.sHeightMapHandleHighRes,GRASS_SHADER_ID,"u_HeightMap");
+	mGrassScene->setTexture(environmentMetaData.sNormalMapHandleHighRes,GRASS_SHADER_ID,"u_NormalMap");
 
 
     /** Noise **/
     GLuint grassNoiseTexture;
     LoadTGATextureSimple((char*)"textures/noise.tga", &grassNoiseTexture);
     mGrassScene->setTexture(grassNoiseTexture,GRASS_SHADER_ID,"u_GrassNoise");
+
+    	/** Set model to world transform **/
+	/** Scale and transform terrain to be centered around zero. And sceled to fit the size of the environment **/
+    vec3 translation =vec3(-environmentMetaData.sSize[0],-environmentMetaData.sSize[1],-environmentMetaData.sSize[2])*0.5;
+    vec3 scale = vec3(environmentMetaData.sSize[0],environmentMetaData.sSize[1],environmentMetaData.sSize[2]);
+    mat4 transf = T(translation.x,translation.y,translation.z)*S(scale.x,scale.y,scale.z);
+	mGrassScene->setTransform(transf,GRASS_SHADER_ID);
 }
 /*
 void GrassSimulation::setEnvironment(Environment * environment){
